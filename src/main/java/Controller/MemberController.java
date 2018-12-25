62ce8ea633487cc77fe8c3f926e57549eca56d40
lintaoko <1244275215@qq.com>
2018-12-25 12:49:56 +0800

    test

--- src/main/java/Controller/MemberController.java
+++ src/main/java/Controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 //@restController 表示该类所有方法返回json 格式 =@App.Controller +@ResponseBody
 public class MemberController {
 
+    //test
     @RequestMapping("/abc")
     public String memberIndex(){
         return "abcdefg";
